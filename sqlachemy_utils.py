ad56c041d3e003e91e441acd35e6fd5bf54810b8
Pablo Berrilio <pabloberrilio@gmail.com>
2014-05-06 14:01:53 -0300

    Add sqlalchemy utils: Automatic commiter and closer of sessions using with

---
+++ sqlachemy_utils.py
@@ -0,0 +1,38 @@
+'''
+Created on 05/05/2014
+
+@author: pablin
+'''
+from contextlib import contextmanager
+
+@contextmanager
+def Session_Commiter(SessionFactory):
+    """Provide a transactional scope around a series of operations.The 
+    SessionFactory must be a class constructed from a sessionmaker.
+    After it goes out of scope it commit the transactions (or rollback 
+    if can not commit (error))."""
+    
+    session = SessionFactory()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+@contextmanager
+def Session_Closer(SessionFactory):
+    """ SessionFactory must be a class constructed from a sessionmaker.
+    After it goes out of scope it close session."""
+    
+    session = SessionFactory()
+    try:
+        yield session
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
