68f7ff3adaeb02abd84ae3841f9aee11e3dd8d1f
Martin Marrese <martin@motrixi.com>
2014-06-04 15:31:48 -0300

    New database models and migrations
    * Add models definition
    * Add migrations using alembic
    * Add a Vagrant example vm configuration file and its bootstrap method.
    * Update README with some comments about alembic, and the directory structure.

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+sudo apt-get update
+
+echo mysql-server-5.5 mysql-server/root_password password "" | sudo debconf-set-selections
+echo mysql-server-5.5 mysql-server/root_password_again password "" | sudo debconf-set-selections
+sudo apt-get install -y mysql-server mysql-client python-mysqldb python-pip
+
+sudo pip install -U pip
+sudo pip install alembic
+
+result=$(mysql -u root -s -N -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='tests'");
+if [ -z "$result" ];
+then
+    echo 'CREATING tests DATABASE'
+    mysql -u root -e "CREATE DATABASE tests CHARACTER SET utf8;"
+else
+    echo 'NO NEED TO CREATE tests DATABASE'
+fi
+
+echo "export PYTHONPATH=/vagrant" >> /home/vagrant/.bashrc
---
+++ src/models.py
@@ -0,0 +1,96 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Table, Column
+from sqlalchemy import ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship, backref
+
+Base = declarative_base()
+
+
+User_Brand = Table('user_brand',
+                   Base.metadata,
+                   Column('id', Integer, primary_key=True),
+                   Column('user_id', Integer, ForeignKey('user.id')),
+                   Column('brand_id', Integer, ForeignKey('brand.id')),
+                   )
+
+
+class Role(Base):
+    __tablename__ = 'role'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(length=45), nullable=False)
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(length=255), nullable=False)
+    passhash = Column(String(length=255), nullable=False)
+    first_name = Column(String(length=255), nullable=False)
+    last_name = Column(String(length=255), nullable=False)
+    token = Column(String(length=255), nullable=True)
+    status = Column(Integer, nullable=True)
+    created = Column(DateTime(), nullable=False)
+    skype_id = Column(String(length=255), nullable=True)
+
+    role_id = Column(Integer, ForeignKey('role.id'))
+    role = relationship("Role", backref=backref('users', order_by=id))
+
+    brands = relationship('Brand', secondary=User_Brand, backref='Brand')
+
+
+class Brand(Base):
+    __tablename__ = 'brand'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(length=45), nullable=False)
+    domain = Column(String(length=255), nullable=False)
+    created = Column(DateTime(), nullable=False)
+
+    agency_id = Column(Integer, ForeignKey('agency.id'))
+    agency = relationship("Agency", backref=backref('brand', order_by=id))
+
+    users = relationship('User', secondary=User_Brand, backref='User')
+
+
+class BrandOptions(Base):
+    __tablename__ = 'brand_options'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    key = Column(String(length=45), nullable=False)
+    value = Column(String(length=45), nullable=True)
+
+    brand_id = Column(Integer, ForeignKey('brand.id'))
+    brand = relationship("Brand", backref=backref('options', order_by=id))
+
+
+class Agency(Base):
+    __tablename__ = 'agency'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(length=45), nullable=False)
+    domain = Column(String(length=255), nullable=False)
+    type = Column(String(length=45), nullable=False)
+    created = Column(DateTime(), nullable=False)
+
+
+class AgencyOptions(Base):
+    __tablename__ = 'agency_options'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    key = Column(String(length=45), nullable=False)
+    value = Column(String(length=45), nullable=True)
+
+    agency_id = Column(Integer, ForeignKey('agency.id'))
+    agency = relationship("Agency", backref=backref('options', order_by=id))
+
+
+class AgencyInfo(Base):
+    __tablename__ = 'agency_info'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    address = Column(String(length=255), nullable=False)
+    address2 = Column(String(length=255), nullable=False)
+    city = Column(String(length=255), nullable=False)
+    state = Column(String(length=30), nullable=True)
+    postcode = Column(String(length=30), nullable=True)
+    country = Column(String(length=45), nullable=False)
+    phone = Column(String(length=15), nullable=True)
+    contact = Column(String(length=100), nullable=True)
+
+    agency_id = Column(Integer, ForeignKey('agency.id'))
+    agency = relationship("Agency", backref=backref('options', order_by=id))
