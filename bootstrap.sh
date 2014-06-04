7c25919fe68c3641d84aff373978f3f1a17df6dd
Martin Marrese <martin@motrixi.com>
2014-06-04 17:36:40 -0300

    Add python-dev to bootstrap, used when copiling sqlalchemy internals

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 
 echo mysql-server-5.5 mysql-server/root_password password "" | sudo debconf-set-selections
 echo mysql-server-5.5 mysql-server/root_password_again password "" | sudo debconf-set-selections
-sudo apt-get install -y mysql-server mysql-client python-mysqldb python-pip
+sudo apt-get install -y mysql-server mysql-client python-mysqldb python-pip python-dev
 
 sudo pip install -U pip
 sudo pip install alembic
